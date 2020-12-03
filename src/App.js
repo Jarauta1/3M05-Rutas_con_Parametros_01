bc7c6afa301bff21562fabf24563776e29d9b6ee
Diego Jarauta <diegojarauta8@gmail.com>
2020-12-03 20:27:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Link, useParams} from "react-router-dom"
+import {useState} from "react"
+
+function Cabecera(props) {
+   
+  return (<>
+  <h1>Rutas con Parámetros</h1>
+  <p>Estás en el ejercicio 1</p>
+  <ul>
+    <li><Link to="/">Inicio</Link></li>
+    <li><Link to={"/saludo/"+props.nombre}>Saludo</Link></li>
+  </ul>
+  </>)
+}
+
+function Saludo() {
+  const {nombre} = useParams()
+  
+  return (<>
+  <h1>Hola {nombre}</h1>
+  </>)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  let [texto,setTexto] = useState("")
+  
+  function cambiarTexto(e) {
+    setTexto(e.target.value)
+  }
+  
+  
+  return (<BrowserRouter>
+    <Cabecera nombre={texto}/>
+  <Route exact path="/"></Route>
+  <Route exact path="/saludo/:nombre">
+    <Saludo/>
+  </Route>
+  <input type="text" value={texto} onChange={cambiarTexto}/>
+  </BrowserRouter>)
 }
 
 export default App;
